c95e7f2d8941781e0b75c36df1b318585b6e543f
alextidd <alexandrarubytidd@gmail.com>
2021-09-01 16:46:53 +1000

    fix bed_fisher_grouped

--- R/bed_fisher_grouped.R
+++ R/bed_fisher_grouped.R
@@ -8,15 +8,18 @@
 #'
 #' @param bedA A tibble BED with columns: chrom, start, end, ...(metadata cols)...
 #' @param bedB A tibble BED with columns: chrom, start, end, ...(metadata cols)...
-#' @param genome A tibble BED of chromosome sizes with columns: chrom, size
 #' @param bedA_groups A character vector of data variable(s) in bedA upon which to group data and perform the test on each group of intervals separately
+#' @param genome A tibble BED of chromosome sizes with columns: chrom, size
+#' @param ... Expressions that return a logical value upon which to filter the Fisher results table (thresholds for test output values)
 #'
 #' @return The outcome of the Fisher test(s) on the overlaps between bedA and bedB
 #' @export
-bed_fisher_grouped <- function(bedA, bedB, genome, bedA_groups = NULL){
+bed_fisher_grouped <- function(bedA, bedB, bedA_groups = NULL, genome, ...){
   A <- bedA %>% dplyr::group_by(dplyr::across(dplyr::all_of(bedA_groups)))
   B <- bedB
 
   A %>%
-    dplyr::group_modify(~ valr::bed_fisher(.x, B, genome))
+    dplyr::group_modify(~ valr::bed_fisher(.x, B, genome)) %>%
+    dplyr::ungroup %>%
+    dplyr::filter(...)
 }
