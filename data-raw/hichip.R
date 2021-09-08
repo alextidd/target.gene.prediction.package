554dfb001308b68f26a6c70f8bd28a34a929d7c2
alextidd <alexandrarubytidd@gmail.com>
2021-09-08 12:04:04 +1000

    changed contact data

--- data-raw/hichip.R
+++
@@ -1,8 +0,0 @@
-## code to prepare `hichip` dataset goes here
-
-# HiChIP example dataset
-BEDPEFile <- "/working/lab_georgiat/alexandT/target_gene_prediction/data/HiChIP/MCF7/abc-ready/HiChIP.bedpe"
-hichip <- target.gene.prediction.package::import_BEDPE_to_List(bedpefile = BEDPEFile,
-                                                               metadata_cols = c("OldPairID", "value"))
-
-usethis::use_data(hichip, overwrite = TRUE)
