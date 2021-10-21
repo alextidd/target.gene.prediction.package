5af5797daa0caacd9dcea58bdc0b4d02745825d3
alextidd <alexandrarubytidd@gmail.com>
2021-10-21 17:12:05 +1000

    change contact structure

--- R/get_g_level_annotations.R
+++ R/get_g_level_annotations.R
@@ -4,8 +4,8 @@ get_g_level_annotations <- function(txv_master,
 
   g <- list()
 
-  # intersect with DHSs
-  g$g_expression_binary <- enriched$expression %>%
+  # gene expressed
+  g$g_expression <- enriched$expression %>%
     tibble::as_tibble(rownames = "ensg") %>%
     dplyr::inner_join(txv_master %>% dplyr::distinct(ensg))
 
--- R/get_txc_level_annotations.R
+++ R/get_txc_level_annotations.R
@@ -12,12 +12,18 @@ get_txc_level_annotations <- function(txv,
     purrr::map(~ dplyr::summarise(., dplyr::across(where(is.numeric), ~ sum(!is.na(.x)))))
   names(n_multi) <- names(n_multi) %>% gsub("txv_", "txc_n_multi", .)
 
+  # binary - multicontact y/n
+  binary_multi <- n_multi %>%
+    purrr::map(~ dplyr::mutate(., dplyr::across(where(is.numeric), ~ as.numeric(.x > 1))))
+  names(binary_multi) <- names(binary_multi) %>% gsub("multicontact", "multicontact_binary", .)
+
   # sum of loop values between gene and CS
   sum_multi <- multicontact %>%
     purrr::map(~ dplyr::summarise(., dplyr::across(where(is.numeric), ~ sum(.x, na.rm = T))))
   names(sum_multi) <- names(sum_multi) %>% gsub("txv_", "txc_sum_multi", .)
 
   txc <- c(n_multi,
+           binary_multi,
            sum_multi)
 
   # return
